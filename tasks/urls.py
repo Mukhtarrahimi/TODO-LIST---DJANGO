fa23726ceaae42816f75c80f8c7139be5be3542f
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-03 08:23:43 +0430

    task_list.html code

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import add_task, task_list
+from .views import add_task, task_list, delete_task
 
 app_name = "tasks"
 
 urlpatterns = [
-    path("", task_list, name="task_list"),  # لیست تسک‌ها
-    path("add/", add_task, name="add_task"),  # اضافه کردن تسک جدید
+    path("", task_list, name="task_list"), 
+    path("add/", add_task, name="add_task"),
+    path("delete/<int:task_id>",delete_task, name="delete_task" )
 ]
