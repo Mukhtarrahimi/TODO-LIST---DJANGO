c8e35542ece3a79fb3a6c1e92dc1cbfe111d5507
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-03 08:03:47 +0430

    delete_task add to views.py

--- tasks/views.py
+++ tasks/views.py
@@ -17,3 +17,9 @@ def add_task(request):
     else:
         form = TaskForm()
     return render(request, "tasks/add_task.html", {"form": form})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect("tasks:task_list")
+    
