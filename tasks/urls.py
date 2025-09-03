623f86d9ecc54a0a4d87f3adc003b3ad9e12fa28
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-03 08:35:08 +0430

    edit_task.html

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import add_task, task_list, delete_task
+from .views import add_task, task_list, delete_task, edit_task
 
 app_name = "tasks"
 
 urlpatterns = [
     path("", task_list, name="task_list"), 
     path("add/", add_task, name="add_task"),
-    path("delete/<int:task_id>",delete_task, name="delete_task" )
+    path("delete/<int:task_id>",delete_task, name="delete_task" ),
+    path("edit/<int:task_id>", edit_task, name="edit_task")
 ]
